69d7fe2033061ada27492f348117ae7863de10f2
BigChocoMonster <bigchocomonster@gmail.com>
2020-11-08 00:54:20 +0530

    - assigning random coordinates to each letter rendered - assigning random color to background and according opposite color to text and text shadow

--- index.js
+++ index.js
@@ -10,17 +10,27 @@ function* charStream() {
   }
 }
 
+function getRandomXCoordinate() {
+  return 40 - (Math.floor(Math.random() * 25) - 5);
+}
+
+function getRandomYCoordinate() {
+  return context.canvas.height / 2 + 10 - (Math.floor(Math.random() * 25) - 20);
+}
+
 function insertText() {
+  console.log(context);
   const stream = charStream();
 
   let next = stream.next();
   let coordinates = {
-    x: 50,
-    y: context.canvas.height / 2,
+    x: getRandomXCoordinate(),
+    y: getRandomYCoordinate(),
   };
   while (!next.done) {
-    context.fillText(next.value, coordinates.x, coordinates.y);
+    context.strokeText(next.value, coordinates.x, coordinates.y);
     coordinates.x += context.measureText(next.value).width + 20;
+    coordinates.y = getRandomYCoordinate();
     next = stream.next();
   }
 }
@@ -28,6 +38,25 @@ function insertText() {
 function setCaptcha() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   context.font = "50px Poppins";
+
+  // these lines exist to give a distorted feel to the text
+  context.shadowBlur = 2.5;
+  context.shadowOffsetX = 10;
+  context.shadowOffsetY = 10;
+
+  // adding random background color
+  const [r, g, b] = [
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+  ];
+  context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+  context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+  // changing font and shadow to a somewhat opposite color
+  const [or, og, ob] = [255 - r, 255 - r, 255 - r];
+  context.strokeStyle = `rgb(${or}, ${og}, ${ob})`;
+  context.shadowColor = `rgb(${or}, ${og}, ${ob})`;
 
   insertText();
 }
